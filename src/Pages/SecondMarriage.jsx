b7d1e38ff09f261699d175d371d92382226f8f33
Dasari Bhargavi <dasaribhargavi35@gmail.com>
2023-09-07 14:17:17 +0530

    Fixed content in services

--- src/Pages/SecondMarriage.jsx
+++ src/Pages/SecondMarriage.jsx
@@ -24,8 +24,7 @@ function SecondMarriage() {
         <ServiceComponent
             imageSrc="https://2.bp.blogspot.com/-CYn6hd-Xls4/WYc5UMLZdnI/AAAAAAAAIdg/B0JPled64lQN_MaXKfYUoZbypFCySk8egCLcBGAs/s640/Married-widows-free-matrimonial-sites-for-widows-Second-Marriage-Matrimonial-Sites.jpg"
             heading="Second Marriage Services"
-            content="Sachin Kumar has captured various shoots of Bollywood Actors like Arbaaz Khan, Riya Sen, Rohit Reddy, Top TV Actors; Divyanka Tripathi, Shweta Tiwari, Rashmi Desai, Ajaz Khan, Mayank Gandhi, Ada Khan, Karanvir Bohra, Karan Singh Grover and many more."
-            summary="Sachin has done hundreds of pre wedding and other wedding ceremonies shoots of many celebrities and elite families. Sachin and his experienced team are specialized in Candid Wedding, Pre-wedding Shoots, Bridal Portraits, Reception, Engagement, Indian Wedding, Wedding, New Born, Birthday, Kids Portraits and Babies & Kids photography."
+            content="Second Marriage caste hails from the Vaishya caste, which is considered to be a mercantile clan of the Hindu social hierarchy, traditionally involved in trade and commerce."
         />
       </div>
       <Footer/>
